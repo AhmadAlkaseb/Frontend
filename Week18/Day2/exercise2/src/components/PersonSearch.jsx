0b10b4d3e34073f833d6873cb9a49540e3d94726
AhmadAlkaseb <ahmadalkasebpri@icloud.com>
2024-05-04 19:21:49 +0200

    .

--- Week18/Day2/exercise2/src/components/PersonSearch.jsx
+++ Week18/Day2/exercise2/src/components/PersonSearch.jsx
@@ -3,15 +3,17 @@ import React, { useState } from 'react'
 const PersonSearch = () => {
     const [searchPerson,setSearchPerson] = useState('');
     const [personFound,setPersonFound] = useState(null);
+    const [searched, setSearched] = useState(false);
+
     const handleReadPerson = (event) => {
         event.preventDefault();
         fetch(`http://localhost:3001/persons/${searchPerson}`)
-        .then((response) => 
-        response.json()
-        ) 
-        .then(data => (
-            setPersonFound(data)
-        ))
+        .then((response) => response.json()) 
+        .then(data => (setPersonFound(data)))
+        .catch(() => 
+            setPersonFound(null),
+            setSearched(true)
+            );
     }
   return (
     <>
@@ -20,19 +22,20 @@ const PersonSearch = () => {
 
         <button type='button' onClick={handleReadPerson}>Search</button>
     </form>
+
+    {searched && personFound === null && (
+                <p>No person found</p>
+    )}
+
     {personFound && (
         <>
-        
         <p>Id: {personFound.id}</p>
         <p>Age: {personFound.age}</p>
         <p>Fullname: {personFound.fullname}</p>
         <p>Gender: {personFound.gender}</p>
         <p>Email: {personFound.email}</p>
-
         </>
-
     )}
-      
     </>
   )
 }
